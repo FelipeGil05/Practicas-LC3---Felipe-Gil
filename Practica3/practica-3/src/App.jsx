43d5e6d986ba7274dc5691576223d46315a18e1b
Felipe Gil <felipeegil05@gmail.com>
2024-09-11 13:29:56 -0300

    Practica 3

---
+++ Practica3/practica-3/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './Components/Login';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Registro de usuario</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
